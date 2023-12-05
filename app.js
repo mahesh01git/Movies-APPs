9d72db48119687cb0af8b14c55170dccf9aac3c7
mahesh01git <151907025+mahesh01git@users.noreply.github.com>
2023-12-05 23:48:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,3 +22,58 @@ const intilazing = async () => {
 };
 
 intilazing();
+
+const convertMovieObj = (obj) => {
+  return {
+    movieId: obj.movie_id,
+    directorId: obj.director_id,
+    movieName: obj.movie_name,
+    leadActor: obj.lead_actor,
+  };
+};
+
+///API 1
+
+app.get("/movies/", async (request, response) => {
+  const api1 = `
+    SELECT movie_name
+    FROM 
+    movie
+    `;
+  const movieName = await dbObj.all(api1);
+  response.send(movieName.map((i) => convertMovieObj(i)));
+});
+
+/// API 2
+
+app.post("/movies/", async (request, response) => {
+  const { directorId, movieName, leadActor } = request.body;
+  const api2 = `
+    INSERT INTO
+    movie (director_id,movie_name,lead_actor)
+    VALUES (
+        ${directorId},
+        '${movieName}',
+        '${leadActor}'
+    )
+
+    `;
+  await dbObj.run(api2);
+  response.send("Movie Successfully Added");
+});
+
+///API 3
+
+app.get("/movies/:movieId/", async (request, response) => {
+  const { movieId } = request.params;
+  const api3 = `
+    SELECT 
+    *
+    FROM 
+    movie
+    WHERE 
+    movie_id = ${movieId}
+    `;
+  const myArraay = await dbObj.get(api3);
+  response.send(myArraay);
+});
